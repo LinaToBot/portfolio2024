ab929d0c0fe6e74c365f2e866128cc1559044167
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-13 20:02:43 -0600

    comments

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // basePath: "/porfolio",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-};
+// const nextConfig = {
+//   // basePath: "/porfolio",
+//   // output: "export", // <=== enables static exports
+//   // reactStrictMode: true,
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
