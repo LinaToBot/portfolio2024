195749812c35c69b115eea0a83444f6d0f191e55
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-14 00:12:49 -0600

    main layout

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,7 +1,32 @@
+// Libraries.
+import Image from "next/image";
+import Link from "next/link";
+// public
+import vercel from "../../../../public/vercel.svg";
+
 export const Header = () => {
+  const linkedinUrl = "https://www.linkedin.com/in/paulina-i-5a7174ba/";
   return (
-    <>
-      <nav>Soy el header </nav>
-    </>
+    <nav className="flex justify-between w-full p-[.0025rem]">
+      <ul className="flex mr-[1rem]">
+        <li className="space-grotesk">
+          <Link href="/">Lina2Bot</Link>
+        </li>
+      </ul>
+      {/* aqui faltaria adaptaplo a celular  */}
+      <div className="flex justify-between w-[12rem]">
+        <p>Celular</p>
+        <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <Image
+            src={vercel}
+            alt="Vercel Logo"
+            className="dark:invert cursor-pointer"
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+    </nav>
   );
 };
---
+++ src/app/features/Animations.tsx
@@ -0,0 +1,31 @@
+import next from "../../../public/next.svg";
+import Image from "next/image";
+
+export const Animations = () => {
+  return (
+    <section className="flex flex-col h-screen items-center mx-[5rem] md:flex-row md:space-x-4 bg-[#94e38b]">
+      <div className="w-full text-justify md: text-left mb-0 w-2/2">
+        <h1 className="text-[3rem]">Hi there!!</h1>
+        <p>
+          I`m a Front-end Developer and I have combined the skills acquired from
+          my first career and my software experience to enhance the quality and
+          user experience of our products.
+        </p>
+        <p>
+          On this site, you can check out my experience and achievements in
+          development and video game design.
+        </p>
+      </div>
+      <div className=" w-full p-4 md: w-1/2 flex justify-center p-4 ">
+        <Image
+          src={next}
+          alt="Vercel Logo"
+          className="dark:invert cursor-pointer"
+          width={100}
+          height={24}
+          priority
+        />
+      </div>
+    </section>
+  );
+};
