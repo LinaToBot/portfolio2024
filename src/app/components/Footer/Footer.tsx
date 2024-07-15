7020e944fee631ff1a367ac2c072cdc8bf3eef6c
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-15 02:55:08 -0600

    Footer added

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+// Libraries.
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="h-[10vh] flex flex-column justify-center text-[0.7rem]">
+      <p>
+        Icons by
+        <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">
+          icons8.com
+        </a>
+      </p>
+    </footer>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@
 import { Intro } from "./features/Intro";
 import { ContactMe } from "./features/ContactMe";
 import { ProjectsSection } from "./features/ProjectsSection";
+import { Footer } from "./components/Footer/Footer";
 
 // bg-[#cccc] md: mx-[10rem] my-[2rem]
 export default function Home() {
   return (
-    <main className=" bg-[#000] flex min-h-screen flex-col items-center justify-between mx-[3.5rem] h-[100%]">
-      <Intro />
-      <ProjectsSection />
-      <ContactMe />
-    </main>
+    <>
+      <main className=" bg-[#000] flex min-h-screen flex-col items-center justify-between mx-[3.5rem] h-[100%]">
+        <Intro />
+        <ProjectsSection />
+        <ContactMe />
+      </main>
+      <Footer />
+    </>
   );
 }
