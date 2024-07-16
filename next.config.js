7643c5f89d6a78c11f2f3f7f3c45762fd86c9e50
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-15 21:47:55 -0600

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/porfolio",
-  output: "export", // <=== enables static exports
   reactStrictMode: true,
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
